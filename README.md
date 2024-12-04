983301502c75253ad4a6b2964f934450d0be07bf
Eloiza <eloiza.souza@zup.com.br>
2024-12-04 16:05:39 -0300

    feat: add input name and birth date

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zup - Catalisa - M02-A04 - Exercício bônus
+
+Crie um programa que recebe como input o nome de uma pessoa e a data de nascimento. 
+A partir desses dados, o programa deve calcular quantos anos a pessoa tem.
+A saída deve ser o nome que a pessoa digitou e a idade dessa pessoa.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("* * * CALCULADORA DE IDADE * * *");
+        System.out.print("Nome: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Nascimento\nDia: ");
+        int birthDay = scanner.nextInt();
+
+        System.out.print("Mês: ");
+        int birthMonth = scanner.nextInt();
+        System.out.print("Ano: ");
+        int birthYear = scanner.nextInt();
+        System.out.printf("%s tem %02d/%02d/%04d anos. %n", name, birthDay, birthMonth, birthYear);
+
+        scanner.close();
     }
 }
